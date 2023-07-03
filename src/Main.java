a7587949ed63808a9c580f4bd7d000b9ebc70b68
lesvladut27 <127212103+lesvladut27@users.noreply.github.com>
2023-07-03 21:17:55 +0300

    add Customer JDBC impl

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import presentation.UserInterface;
 public class Main {
     public static void main(String[] args) {
 
+
         DataAccessObject dataAccessObject = new DataAccessObject();
         ManagementService managementService = new ManagementService(dataAccessObject);
         UserInterface userInterface = new UserInterface(managementService);
--- src/persistence/CustomerDaoSQLImp.java
+++ src/persistence/CustomerDaoSQLImp.java
@@ -1,44 +1,47 @@
 package persistence;
 
+import business.CustomerService;
 import business.entities.Customer;
 import util.DBUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class CustomerDaoSQLImp implements CustomerDao{
+public class CustomerDaoSQLImp implements CustomerDao {
 
     @Override
     public List<Customer> getAllCustomers() {
-        List<Customer> customers = new ArrayList<>();
-        Connection connection= null;
+        List<Customer> customer = new ArrayList<>();
+        Connection connection = null;
         Statement statement = null;
-        ResultSet rs = null
+        ResultSet rs = null;
         try {
             connection = DBUtil.getConnection();
             statement = connection.createStatement();
-            rs = statement.executeQuery("select * from customers");
+            rs = statement.executeQuery("select * from customer");
             while (rs.next()) {
                 int id = rs.getInt("id");
-                String name = rs.getString(2);
-                String date_of_birth = rs.getDate(3);
-                String phone_number = rs.getString(4);
+                String first_name = rs.getString(2);
+                String last_name = rs.getString(3);
+                LocalDate date_of_birth = rs.getDate(4).toLocalDate();
+                String phone_number = rs.getString(5);
                 String email = rs.getString("email");
-                String reservation_id = rs.getInt(6);
+                int reservation_id = rs.getInt(7);
 
-                Customer customer = new Customer(id, name, date_of_birth, phone_number,email,reservation_id);
-                customer.add(customer);
-                }
-        }catch (SQLException e) {
+                Customer customer1 = new Customer(id, first_name, last_name, date_of_birth, phone_number, email, reservation_id);
+                customer.add(customer1);
+            }
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
             DBUtil.closeConnection(connection);
         }
-        return customers;
+        return customer;
 
     }
+
 }
+
